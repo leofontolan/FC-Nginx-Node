68c57115dfd6ebcce4dcc5ac893e0762acd66022
Leo Fontolan <leofontolan@gmail.com>
2021-12-30 18:55:09 -0300

    template html pug

--- node/index.js
+++ node/index.js
@@ -1,34 +1,59 @@
+//----------------------------------------------------------> Criar Rota Express
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => {
+app.set('view engine', 'pug')
 
-    res.send('<h1>Desafio Full Cycle | Nginx Node')
+app.get('/', function(req, res, next) {
+
+    var sql="SELECT * FROM people where id= '1'";
+    connection.query(sql, function (err, data, fields) {
+    if (err) throw err;
+    res.render('index', { title: 'Full Cicly', message: 'Desafio Full Cycle | Nginx + NodeJs + MySQL',userName:  Object.values(JSON.parse(JSON.stringify(data)))[0].name } );
+  });
+
+});
+
+app.listen(port, () => {
+    console.log('Rodando ' + port)
 })
 
-app.listen(port, ()=> {
-    console.log ('Rodando ' + port)
-})
 
 
-
+//----------------------------------------------------------> Conectar MySQL
+const config = {
+    host: "db",
+    user: "root",
+    password: "root",
+    database: "nodedb"
+}
 
 const mysql = require('mysql');
+const connection = mysql.createConnection(config);
 
-const con = mysql.createConnection({
-  host: "db",
-  user: "root",
-  password: "root",
-  database: "nodedb"
-});
+//----------------------------------------------------------> CRUD
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  const sql = "CREATE TABLE people (id INT NOT NULL auto_increment, name VARCHAR(255), PRIMARY KEY(id) )";
-  con.query(sql, function (err, result) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("Table created");
-  });
+    console.log("Conectado!");
+
+    const sql = "CREATE TABLE IF NOT EXISTS people (id INT NOT NULL auto_increment, name VARCHAR(255), PRIMARY KEY(id) )";
+
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Tabela Criada");
+    });
+
+
+    connection.query(`INSERT INTO people(name) values('Leo Fontolan')`, (err, res) => {
+        if(err) throw err;
+        console.log('OK INSERT');
+      });
+
+    //   connection.query(`SELECT * FROM people where id= '1'`, (err, rows) => {
+    //     if(err) throw err;
+    //     console.log(   Object.values(JSON.parse(JSON.stringify(rows)))[0].name    );
+    //   });
 });
+
